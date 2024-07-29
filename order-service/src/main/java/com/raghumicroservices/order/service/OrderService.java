3ba67c7b6b4f27bbf67f62b2a2e0bcf05824cdab
RAGHUNANDANn <nandanr608@gmail.com>
2024-07-29 10:59:25 +0530

    feat:Added fiegn client communication and mocer it using wiremock

--- order-service/src/main/java/com/raghumicroservices/order/service/OrderService.java
+++ order-service/src/main/java/com/raghumicroservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.raghumicroservices.order.service;
 
+import com.raghumicroservices.order.client.InventoryClient;
 import com.raghumicroservices.order.dto.OrderRequest;
 import com.raghumicroservices.order.model.Order;
 import com.raghumicroservices.order.repository.OrderRepository;
@@ -15,15 +16,24 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
+
 
     public void placeOrder(OrderRequest request){
-        Order order=Order.builder()
-                .orderNumber(UUID.randomUUID().toString())
-                .skuCode(request.skuCode())
-                .price(request.price())
-                .quantity(request.quantity())
-                .build();
-        orderRepository.save(order);
-        log.info("Order placed");
+
+       boolean isProductInStock= inventoryClient.isInStock(request.skuCode(), request.quantity());
+if(isProductInStock){
+    Order order=Order.builder()
+            .orderNumber(UUID.randomUUID().toString())
+            .skuCode(request.skuCode())
+            .price(request.price())
+            .quantity(request.quantity())
+            .build();
+    orderRepository.save(order);
+    log.info("Order placed");
+}else {
+    log.info("Order not in stock");
+    throw new RuntimeException("Product with SKUCODE"+request.skuCode()+"is  not in stock");
+}
     }
 }
